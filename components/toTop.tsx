c0fc7f496ad6b14b79370ebe99ce41963c41948d
caseBread <kgu0515@gmail.com>
2023-02-19 14:55:10 +0900

    feat : top button fadein/fadeout 추가

--- components/toTop.tsx
+++ components/toTop.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
 import { AiFillCaretUp } from "react-icons/ai";
 
 const ToTop = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  const handleFollow = () => {
+    setShowTopBtn(window.pageYOffset > 20);
+  };
+
+  useEffect(() => {
+    const scrollWatch = () => {
+      window.addEventListener("scroll", handleFollow);
+    };
+    scrollWatch();
+    return () => window.removeEventListener("scroll", handleFollow);
+  });
+
   const handleTop = () => {
     window.scrollTo({
       top: 0,
@@ -8,13 +23,18 @@ const ToTop = () => {
     });
   };
 
+  // TODO : Top button 나타날때 위에서 나타나도록 / 사라질때도 아래로 사라지기
   return (
-    <button
-      className="fixed bottom-8 right-8 border-2 p-4 rounded-full bg-white hover:bg-secondary hover:border-secondary z-50"
-      onClick={handleTop}
-    >
-      <AiFillCaretUp />
-    </button>
+    <>
+      <button
+        className={`fixed  right-8 border-2 p-4 rounded-full bg-white hover:bg-secondary hover:border-secondary z-50 duration-300 ${
+          showTopBtn ? "bottom-8" : "-bottom-14"
+        }`}
+        onClick={handleTop}
+      >
+        <AiFillCaretUp />
+      </button>
+    </>
   );
 };
 export default ToTop;
